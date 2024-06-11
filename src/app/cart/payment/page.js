5f122c316ef1d556b64c371b6f7d10c3f3e9f858
test <jakub.koci@educanet.cz>
2024-06-11 18:54:06 +0200

    Custom item shop with shopping cart

--- src/app/cart/payment/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page(){
-    return(
-        <div className={"my-80"}>
-            coming soon..
-        </div>
-    )
-}
--- src/redux/selectors.js
+++
@@ -1,5 +0,0 @@
-export const selectCartItems = (state) => state.cart.cartItems;
-
-export const selectTotalPrice = (state) => {
-    return state.cart.cartItems.reduce((total, item) => total + item.price, 0);
-};
